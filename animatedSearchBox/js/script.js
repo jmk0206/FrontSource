bb8568af65e1b56178ea4387eedebc597a0977aa
jmk0206 <jmk960206@gmail.com>
2022-09-25 20:22:03 +0900

    Search Box

---
+++ animatedSearchBox/js/script.js
@@ -0,0 +1,14 @@
+let search = document.querySelector('.search');
+let close = document.querySelector('.close');
+let searchBox = document.querySelector('.searchBox');
+
+search.onclick = function() {
+	searchBox.classList.add('active');
+}
+
+close.onclick = function() {
+	searchBox.classList.remove('active');
+}
+
+
+
